9ffd91345667708d07ee1b30ab9da3c66de8bada
cif95 <vivianacifarelli9@gmail.com>
2022-04-19 16:32:27 +0200

    created html structure

--- index.php
+++ index.php
@@ -9,13 +9,32 @@ Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 	$badWord = $_GET["badword"];
 ?>
 
-<p>
-	<?php
-		echo str_replace($badWord, "***", $paragraph);
-	?>
-	<pre>
-		<?php
-			echo " => la lunghezza del paragrafo è di " . strlen($paragraph) . " caratteri";
-		?>
-	</pre>
-</p>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./style.css">
+	<title>Php Badwords</title>
+</head>
+<body>
+	<main>
+		<section>
+			<q>
+				<?php
+					echo str_ireplace($badWord, "***", $paragraph);
+				?>
+			</q>
+			<span id="p-lenght">
+				<?php
+					echo "La lunghezza del paragrafo è di " . strlen($paragraph) . " caratteri.";
+				?>
+			</span>
+			<span>
+				Le parole censurate verranno visualizzate con tre asterischi (inserire una parola da censurare tramite parametro get : ?badword=x ).
+			</span>
+		</section>
+	</main>
+</body>
+</html>
